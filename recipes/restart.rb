874ce8b86162e1e1e2ee6a7c68deada22abbc71d
Franklin Wise <franklin@krave-n.com>
2014-11-22 21:13:08 -0800

    stop start

--- recipes/restart.rb
+++ recipes/restart.rb
@@ -2,7 +2,13 @@
 case node[:zookeeper][:service_style]
 when 'runit'
   runit_service 'zookeeper' do
-    action [:restart]
+    action [:stop]
+  end
+
+  sleep 5
+
+  runit_service 'zookeeper' do
+    action [:start]
   end
 when 'exhibitor'
   Chef::Log.info("Assuming Exhibitor will start up Zookeeper.")
