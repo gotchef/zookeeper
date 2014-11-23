3d80e07dd10154777810c101c507fd75ef5c8e9e
Franklin Wise <franklin@krave-n.com>
2014-11-22 21:05:24 -0800

    simplified restart

--- recipes/restart.rb
+++ recipes/restart.rb
@@ -1,16 +1,7 @@
 
-executable_path = ::File.join(node[:zookeeper][:install_dir],
-                              "zookeeper-#{node[:zookeeper][:version]}",
-                              'bin',
-                              'zkServer.sh')
-
 case node[:zookeeper][:service_style]
 when 'runit'
   runit_service 'zookeeper' do
-    default_logger true
-    options({
-      exec: executable_path
-    })
     action [:restart]
   end
 when 'exhibitor'
