1beaf947c0337b3b0e170785ee6fd415c1f8a427
Franklin Wise <franklin@krave-n.com>
2015-02-09 18:26:56 -0800

    updated version

--- metadata.rb
+++ metadata.rb
@@ -3,15 +3,10 @@ maintainer       'Krave-n, Inc.'
 maintainer_email 'dev-ops@krave-n.com'
 license          'Copyright 2014, Krave-n, Inc'
 description      'Installs/Configures zookeeper'
-version          '2.4.1'
+version          '0.1'
 
 depends          'build-essential', '>= 2.0.2'
 depends          'java'
 depends          'runit'
-
-recipe           "zookeeper::service",                 "Installs zookeeper as a service that always runs"
-recipe           "zookeeper::default",                 "Installs Zookeeper server, sets up and starts service"
-recipe           "zookeeper::install",                 "Install zookeeper"
-recipe           "zookeeper::configure",            "Config files -- include this last after discovery"
 
 
